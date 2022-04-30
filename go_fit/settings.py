ac4ffeca02992a6a28b04d9777a5101e423f75fb
conorwalsh1 <conorwalsh7@gmail.com>
2022-04-30 08:23:44 +0000

    Deployment commit

--- go_fit/settings.py
+++ go_fit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -103,11 +104,15 @@ WSGI_APPLICATION = 'go_fit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://skzvkthgphouhe:f9f58305a4212378faae6819b58cc6380a0c06768031e5010ff7f735a0582890@ec2-54-170-90-26.eu-west-1.compute.amazonaws.com:5432/d5ao8in2rkknua')
 }
 
 
